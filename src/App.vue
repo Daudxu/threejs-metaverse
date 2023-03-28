bbe935c0d16c10a7a5e24d6b1ab0a59edc11351d
Dan <875126243@qq.com>
2023-03-28 23:33:28 +0800

    fix update

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,120 @@
-<script setup>
-import * as THREE from 'three'
-import  Stats  from "three/examples/jsm/libs/stats.module"
-import  { Octree }  from "three/examples/jsm/math/Octree.js"
-import { Capsule } from "three/examples/jsm/math/Capsule.js"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import { OctreeHelper } from "three/examples/jsm/helpers/OctreeHelper.js";
-import { onMounted, reactive } from 'vue'
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
-import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js"
-onMounted (()=>{
-    const clock = new THREE.Clock();
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x88ccee);
-    scene.fog = new THREE.Fog(0x88ccee, 0, 50);
-
-    const camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.001,
-      1000
-    )
-    camera.position.set(0, 5, 10)
-    
-    const backCamera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.001,
-      1000
-    );
-    backCamera.position.set(0, 5, -10);
-
-    const container = document.getElementById("container");
-    const renderer = new THREE.WebGL1Renderer({ antialias: true })
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.VSMShadowMap;
-    renderer.outputEncoding = THREE.sRGBEncoding;
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    container.appendChild(renderer.domElement)
-  
-    const stats = new Stats();
-    stats.domElement.style.position = "absolute";
-    stats.domElement.style.top = '0px';
-    container.appendChild(stats.domElement);
-    console.log(stats)
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 0, 0);
-    let activeCamera = camera
-    function animate() {
-      let delta = clock.getDelta();
-      stats.update();
-      controls.update();
-      renderer.render(scene, activeCamera);
-   
-      requestAnimationFrame(animate);
-    }
-
-    // 创建一个平面
-    const planeGeometry = new THREE.PlaneGeometry(20, 20, 1, 1);
-    const planMaterial = new THREE.MeshBasicMaterial({
-      color: 0xffffff,
-      side: THREE.DoubleSide
-    });
-    const plane = new THREE.Mesh(planeGeometry, planMaterial);
-    plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2;
-    scene.add(plane)
-
-    // const geometry = new THREE.TorusKnotBufferGeometry(1, 0.3, 100, 16);
-    const material = new THREE.MeshBasicMaterial({ color: 0x22ff00 });
-    let geometryes = [];
-    for(let i = 0; i<1000; i++){
-      const geometry = new THREE.TorusKnotBufferGeometry(
-        0.5 + Math.random() * 0.5,
-        0.3,
-        50+ parseInt(Math.random() * 50),
-        16
-      )
-      const matrix = new THREE.Matrix4();
-      matrix.makeRotationX(Math.random() * Math.PI);
-      matrix.makeRotationY(Math.random() * Math.PI);
-      matrix.makeRotationZ(Math.random() * Math.PI);
-      matrix.makeScale(
-        Math.random() * 0.5 + 0.5,
-        Math.random() * 0.5 + 0.5,
-        Math.random() * 0.5 + 0.5
-      )
-      matrix.makeTranslation(
-        Math.random() * 100 - 50,
-        Math.random() * 100 - 50,
-        Math.random() * 100 - 50
-      )
-      geometry.applyMatrix4(matrix)
-      geometryes.push(geometry)
-    }
-    const mergeGeometry = BufferGeometryUtils.mergeBufferGeometries(geometryes)
-    let mesh = new THREE.Mesh(mergeGeometry, material)
-    scene.add(mesh)
-    animate();
-});
-
-
-</script>
-
 <template>
-  <div id="container">
-  </div>
+  <div id="container"></div>
 </template>
 
-<style scoped>
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import * as THREE from "three";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+import { Octree } from "three/examples/jsm/math/Octree.js";
+import { OctreeHelper } from "three/examples/jsm/helpers/OctreeHelper.js";
+
+import { Capsule } from "three/examples/jsm/math/Capsule.js";
+
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+import { reactive, onMounted, ref } from "vue";
+
+import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+
+onMounted(() => {
+  const clock = new THREE.Clock();
+
+  const scene = new THREE.Scene();
+
+  scene.background = new THREE.Color(0x88ccee);
+  // scene.fog = new THREE.Fog(0x88ccee, 0, 50);
+
+  const camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.001,
+    1000
+  );
+  camera.position.set(0, 5, 10);
+
+  const container = document.getElementById("container");
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.VSMShadowMap;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  container.appendChild(renderer.domElement);
+
+  const stats = new Stats();
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.top = "0px";
+  container.appendChild(stats.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0, 0);
+
+  console.log(renderer.info);
+
+
+  function animate() {
+    let delta = clock.getDelta();
+
+    stats.update();
+    controls.update();
+    cubeCamera.update(renderer, scene);
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  }
+
+  const hdrLoader = new RGBELoader();
+  hdrLoader.load("./hdr/023.hdr", (texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    texture.format = THREE.RGBAFormat;
+    scene.background = texture;
+    scene.environment = texture;
+    sphereMaterial.envMap = cubeRenderTarget.texture;
+  });
+
+  // 创建球
+  const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+  const sphereMaterial = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff,
+    // 透光程度
+    transparent: true,
+    roughness: 0,
+    metalness: 1,
+  });
+  sphereMaterial._iridescence = 1;
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.set(0, 0, 0);
+  scene.add(sphere);
+
+  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const boxMaterial = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff
+  })
+  const box = new THREE.Mesh(boxGeometry, boxMaterial)
+  box.position.set(3, 0, 0)
+  scene.add(box)
+
+  // 创建 cubeTarget
+  const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512);
+  const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget);
+  animate();
+});
+</script>
+
+<style>
+* { 
+  margin: 0;
+  padding: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+#container {
+  width: 100vw;
+  height: 100vh;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
 </style>

793136d6e9099e775f7054a0c43664692e4a3467
xudan <875126243@qq.com>
2023-03-30 17:04:35 +0800

    fix update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="container"></div>
+  <div class="cl-option">
+    <button @click="handleClickAddAvatar"> dd Avatar</button>
+  </div>
 </template>
 
 <script setup>
@@ -18,14 +21,19 @@ import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import { reactive, onMounted, ref } from "vue";
-
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils'
 import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3000');
+const clock = new THREE.Clock();
+const scene = new THREE.Scene();
+
 
 onMounted(() => {
-  const clock = new THREE.Clock();
 
-  const scene = new THREE.Scene();
+
 
   scene.background = new THREE.Color(0x88ccee);
   // scene.fog = new THREE.Fog(0x88ccee, 0, 50);
@@ -57,7 +65,7 @@ onMounted(() => {
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.target.set(0, 0, 0);
 
-  console.log(renderer.info);
+  // console.log(renderer.info);
 
 
   function animate() {
@@ -93,19 +101,59 @@ onMounted(() => {
   sphere.position.set(0, 0, 0);
   scene.add(sphere);
 
+  var box1 = createAvatar()
+  box1.position.set(Math.random() * 10 - 2, 0, Math.random() * 10 - 2)
+  scene.add(box1)
+
+  // 创建 cubeTarget
+  const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512);
+  const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget);
+  // 添加角色到场景
+  socket.on("connect", () => {
+    console.log('ok'); 
+    // socket.emit("connect", "world");
+    // socket.emit('connect', 'a');
+  });
+  socket.on("message", (e) => {
+    // console.log(e); 
+    if(typeof e === 'object') {
+      console.log(e); 
+      // var m = SkeletonUtils.clone(scene)
+      // var  position = new THREE.Vector3(e.x, e.y, e.z)
+      const box = createAvatar()
+      box.position.set(e.x, e.y, e.z)
+      // box.position.set(position)
+      scene.add(box)
+    }
+
+    // var object3D = new THREE.Object3D();
+    //  object3D.add(e)
+    // scene.add(object3D)
+    // socket.emit("connect", "world");
+    // socket.emit('message', 'a');
+  });
+
+  animate();
+});
+const handleClickAddAvatar = () => {
+  // console.log("===")
+  var avatar = createAvatar()
+  avatar.position.set(Math.random() * 10 - 2, Math.random() * 10 - 2, Math.random() * 10 - 2)
+  socket.emit("message", avatar.position);
+  scene.add(avatar)
+
+}
+
+const createAvatar = () => {
   const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
   const boxMaterial = new THREE.MeshPhysicalMaterial({
     color: 0xffffff
   })
   const box = new THREE.Mesh(boxGeometry, boxMaterial)
-  box.position.set(3, 0, 0)
-  scene.add(box)
-
-  // 创建 cubeTarget
-  const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512);
-  const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget);
-  animate();
-});
+  // var object3D = new THREE.Object3D();
+  // object3D.add(box)
+  return box
+}
 </script>
 
 <style>
@@ -116,5 +164,10 @@ onMounted(() => {
 #container {
   width: 100vw;
   height: 100vh;
+}
+.cl-option {
+  position: fixed;
+  right: 0;
+  top: 0;
 }
 </style>

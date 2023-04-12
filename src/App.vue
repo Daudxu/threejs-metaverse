d0f841ae4a0a5489234effa214033c62412f8476
xudan <875126243@qq.com>
2023-04-12 17:25:50 +0800

    fix update

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,20 @@ import  Stats  from "three/examples/jsm/libs/stats.module"
 import  { Octree }  from "three/examples/jsm/math/Octree.js"
 import { Capsule } from "three/examples/jsm/math/Capsule.js"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-
+import * as CANNON from "cannon-es"
 import { onMounted, reactive } from 'vue'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { threeToCannon, ShapeType } from 'three-to-cannon';
+import CannonDebugger from 'cannon-es-debugger'
 
 onMounted (()=>{
-    const clock = new THREE.Clock();
+
+    // 场景初始化
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x88ccee);
     scene.fog = new THREE.Fog(0x88ccee, 0, 50);
-
+    // 创建相机
     const camera = new THREE.PerspectiveCamera(
       70,
       window.innerWidth / window.innerHeight,
@@ -20,7 +25,7 @@ onMounted (()=>{
       1000
     )
     camera.position.set(0, 5, 10)
-    
+    // 渲染
     const container = document.getElementById("container");
     const renderer = new THREE.WebGL1Renderer({ antialias: true })
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -30,26 +35,91 @@ onMounted (()=>{
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     container.appendChild(renderer.domElement)
-    
+    // 性能监控
     const stats = new Stats();
     stats.domElement.style.position = "absolute";
     stats.domElement.style.top = '0px';
     container.appendChild(stats.domElement);
-    
+
+    // 轨道控制
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(0, 0, 0);
 
-    function animate() {
-      let delta = clock.getDelta();
-      controlsPlay(delta)
-      updatePlayer(delta)
-      resetPlayer()
-      stats.update();
-      // controls.update();
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    }
+    // let dracoLoader = new DRACOLoader()
+    // let loader = new  GLTFLoader()
+    // dracoLoader.setDecoderPath("./draco/gltf/");
+    // dracoLoader.setDecoderConfig({type: "js"});
+    // loader.setDRACOLoader(loader);
 
+    // 创建物理世界
+    const world = new CANNON.World();
+    world.gravity.set(0, -100, 0);
+    const cannonDebugger = new CannonDebugger(scene, world, {
+      // options...
+    })
+
+ 
+    let robotModel = null
+    const gltfLoader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("./draco/gltf/");
+    dracoLoader.setDecoderConfig({ type: "js" });
+    dracoLoader.preload();
+    gltfLoader.setDRACOLoader(dracoLoader);
+    gltfLoader.load('./models/metaScene03.glb', gltf => {
+      console.log("gltf", gltf)
+      //阴影
+      gltf.scene.traverse(function (object) {
+     
+        if (object.isMesh) object.castShadow = true;
+        if(object.isMesh){
+          console.log(object.geometry)
+          if(object.name === "KB3D_DLA_BldgMD_H_BaseA_1"){
+            object.material.wireframe = true
+            // var shape = new CANNON.Trimesh(object.geometry);
+            var shape = new CANNON.Heightfield(object.geometry);
+            var body = new CANNON.Body({ mass: 1 });
+            body.addShape( shape );
+            // 将物体添加至物理世界
+            world.addBody(body);
+          }
+        }
+      });
+      robotModel = gltf.scene
+      // gltf.scene.name = config.name
+      // gltf.scene.rotation.set(0, 0, 0)
+      // resolve(gltf)
+      scene.add(robotModel)
+    })
+
+    // 创建物理小球形状
+    const sphereShape = new CANNON.Sphere(0.5);
+    // 设置物体材质
+    // const sphereWorldMaterial = new CANNON.Material();
+    // 创建物理世界的物体
+    const sphereBody = new CANNON.Body({
+        shape: sphereShape,
+        // 位置
+        position: new CANNON.Vec3(0, 5, 0),
+        // 小球质量
+        mass: 1,
+        // 物体材质
+        // material: sphereWorldMaterial,
+    });
+    // 将物体添加至物理世界
+    world.addBody(sphereBody);
+    // 创建地面
+    const floorShape = new CANNON.Plane();
+    const floorBody = new CANNON.Body();
+    // 当质量为0时，使得物体保持不动
+    floorBody.mass = 0;
+    floorBody.addShape(floorShape);
+    // 位置
+    floorBody.position.set(0, 0, 0);
+    // floorBody.position.x = -Math.PI / 2;
+    // 旋转
+    floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+    world.addBody(floorBody);
     // 创建一个平面
     const planeGeometry = new THREE.PlaneGeometry(20, 20, 1, 1);
     const planMaterial = new THREE.MeshBasicMaterial({
@@ -59,189 +129,50 @@ onMounted (()=>{
     const plane = new THREE.Mesh(planeGeometry, planMaterial);
     plane.receiveShadow = true;
     plane.rotation.x = -Math.PI / 2;
+    // plane.position.set(0, 0, 0);
+    // plane.position.copy(floorBody.position)
+    scene.add(plane)
 
-    // 创建一个octree
-    const group = new THREE.Group();
-    group.add(plane);
-    scene.add(group);
-    const worldOctree = new Octree();
-    worldOctree.fromGraphNode(group);
-
-    // 创建一个人的碰撞体
-    const playerCollider = new Capsule(
-      new THREE.Vector3(0, 0.35, 0),
-      new THREE.Vector3(0, 1.35, 0),
-      0.35
-    );
-    console.log('worldOctree', worldOctree)
-    console.log('playerCollider', playerCollider)
-
-    // 创建胶囊的眼睛
-    const capsuleBodyGeometry = new THREE.PlaneGeometry(1, 0.5, 1, 1);
-    const capsuleBodyMaterial = new THREE.MeshBasicMaterial({
-      color: 0x0000ff,
-      side: THREE.DoubleSide,
-    });
-    const capsuleBody = new THREE.Mesh(capsuleBodyGeometry, capsuleBodyMaterial);
-    capsuleBody.position.set(0, 2, 0.35);
-    // 创建胶囊几何体
-    const capsuleGeometry  = new THREE.CapsuleGeometry(0.35, 1, 32);
-    const capsuleMaterial = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      side: THREE.DoubleSide
-    });
-    const capsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial)
-    capsule.position.set(0, 0.85, 0);
-    capsule.castShadow = true;
-    // camera.position.set(0, 2, -5);
-    // camera.lookAt(capsule.position);
-    // controls.target = capsule.position;
-    // capsule.add(camera)
-    capsule.add(capsuleBody)
-    scene.add(capsule)
-
-    // 设置重力
-    const gravity = -0.85;
-    // 玩家速度
-    const  playerVelocity = new THREE.Vector3(0, 0, 0);
-    // 方向向量
-    const playerDirection = new THREE.Vector3(0, 0, 0); 
-    //  键盘按下事件
-    const keyStates = {
-      KeyW: false,
-      KeyA: false,
-      KeyS: false,
-      KeyD: false,
-      Space: false,
-      isDown: false,
-    };
-
-    // 玩家是否在地面上
-    let playerOnFloor = false;
-    // 更新玩家数据
-    function updatePlayer(deltaTime) {
-      let damping = -0.05;
-      if (playerOnFloor) {
-        playerVelocity.y = 0;
-        keyStates.isDown ||
-        playerVelocity.addScaledVector(playerVelocity, damping);
-      } else {
-        playerVelocity.y += gravity * deltaTime;
-      }
-      // 计算移动距离
-      const playerMoveDistance = playerVelocity.clone().multiplyScalar(deltaTime);
-      playerCollider.translate(playerMoveDistance)
-      // 设置胶囊位置
-      playerCollider.getCenter(capsule.position)
-      // 碰撞检测
-      playerCollisions();
-    }
+    const pointLight1 = new THREE.PointLight(0xffffff,1.0);
+    const pointLight2 = new THREE.PointLight(0xffffff,1.0);
+    pointLight1.position.set(400, 100, 200);
+    pointLight2.position.set(-400, 100, -200);
+    scene.add(pointLight1);
+    scene.add(pointLight2);
+    const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
+    // let pointLightHelper = new THREE.PointLightHelper( pointLight1, 1 );
+    // scene.add( pointLightHelper );
     
-    function playerCollisions() {
-      // 人物碰撞检测
-      const result = worldOctree.capsuleIntersect(playerCollider);
-      // console.log(result);
-      playerOnFloor = false;
-      if (result) {
-        playerOnFloor = result.normal.y > 0;
-        playerCollider.translate(result.normal.multiplyScalar(result.depth));
-      }
-    }
+    // // 创建胶囊几何体
+    // const capsuleGeometry  = new THREE.CapsuleGeometry(0.35, 1, 32);
+    // const capsuleMaterial = new THREE.MeshBasicMaterial({
+    //   color: 0xff0000,
+    //   side: THREE.DoubleSide
+    // });
+    // const capsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial)
+    // capsule.position.set(0, 0.85, 0);
+    // capsule.castShadow = true;
+    // scene.add(capsule)
+    const clock = new THREE.Clock();
 
-    function resetPlayer() {
-      if (capsule.position.y < -20) {
-        playerCollider.start.set(0, 2.35, 0);
-        playerCollider.end.set(0, 3.35, 0);
-        playerCollider.radius = 0.35;
-        playerVelocity.set(0, 0, 0);
-        playerDirection.set(0, 0, 0);
-      }
-    }
-
-    // 更新键盘状态
-    document.addEventListener(
-      "keydown",
-      (event) => {
-        console.log(event.code);
-        keyStates[event.code] = true;
-        keyStates.isDown = true;
-      },
-      false
-    );
-
-    document.addEventListener(
-      "keyup",
-      (event) => {
-        keyStates[event.code] = false;
-        keyStates.isDown = false;
-      },
-      false
-    );
-    // 根据鼠标在屏幕移动，来旋转胶囊
-    // window.addEventListener(
-    //   "mousemove",
-    //   (event) => {
-    //     capsule.rotation.y -= event.movementX * 0.003;
-    //   },
-    //   false
-    // );
-
-    // document.addEventListener(
-    //   "mousedown",
-    //   (event) => {
-    //     // 锁定鼠标指针
-    //     document.body.requestPointerLock();
-    //   },
-    //   false
-    // );
-
-    // 更新键盘状态更新玩家的速度
-    function controlsPlay(deltaTime){
-        if(keyStates['KeyW']){
-          playerDirection.Z = 1;
-          // 获取脚男的正面前面方向
-          const caosuleFront = new THREE.Vector3(0, 0, 0);
-          capsule.getWorldDirection(caosuleFront);
-          // 计算玩家的速度
-          playerVelocity.add(caosuleFront.multiplyScalar(deltaTime))
-        }
-        if (keyStates["KeyS"]) {
-          playerDirection.z = 1;
-          //获取胶囊的正前面方向
-          const capsuleFront = new THREE.Vector3(0, 0, 0);
-          capsule.getWorldDirection(capsuleFront);
-          // console.log(capsuleFront);
-          // 计算玩家的速度
-          playerVelocity.add(capsuleFront.multiplyScalar(-deltaTime));
-        }
-        if(keyStates["KeyA"]) {
-          playerDirection.x = 1;
-          //获取胶囊的正前面方向
-          const capsuleFront = new THREE.Vector3(0, 0, 0);
-          capsule.getWorldDirection(capsuleFront);
-          // 向量积
-          // 侧方的方向，正前面的方向和胶囊的正上方求叉积，求出侧方的方向
-          capsuleFront.cross(capsule.up);
-          playerVelocity.add(capsuleFront.multiplyScalar(-deltaTime));
-        }
-        if(keyStates["KeyD"]) {
-          playerDirection.x = 1;
-          //获取胶囊的正前面方向
-          const capsuleFront = new THREE.Vector3(0, 0, 0);
-          capsule.getWorldDirection(capsuleFront);
-
-          // 侧方的方向，正前面的方向和胶囊的正上方求叉积，求出侧方的方向
-          capsuleFront.cross(capsule.up);
-          playerVelocity.add(capsuleFront.multiplyScalar(deltaTime));
-        }
-        if (keyStates["Space"]) {
-          if(playerVelocity.y === 0) {
-            playerVelocity.y = 3;
-          }
-        }
-    }
-
+    function animate() {
+      let delta = clock.getDelta();
+      world.step(1 / 60, delta, 10);
+      if(robotModel){
     
+        // const result = threeToCannon(robotModel);
+        // console.log('robotModel', result)
+        robotModel.position.copy(sphereBody.position)
+      }
+    
+      // capsule.quaternion.copy(sphereBody.position)
+      stats.update();
+      controls.update();
+      renderer.render(scene, camera);
+      cannonDebugger.update()
+      requestAnimationFrame(animate);
+    }
     animate();
 });
 

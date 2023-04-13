65063675d45d0e7cad657b5f55b3575a9b51acd0
xudan <875126243@qq.com>
2023-04-13 15:05:52 +0800

    fix update

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <script setup>
 import * as THREE from 'three'
 import  Stats  from "three/examples/jsm/libs/stats.module"
-import  { Octree }  from "three/examples/jsm/math/Octree.js"
-import { Capsule } from "three/examples/jsm/math/Capsule.js"
+// import  { Octree }  from "three/examples/jsm/math/Octree.js"
+// import { Capsule } from "three/examples/jsm/math/Capsule.js"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import * as CANNON from "cannon-es"
+// import * as CANNON from "cannon"
 import { onMounted, reactive } from 'vue'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
-import { threeToCannon, ShapeType } from 'three-to-cannon';
+// import { threeToCannon, ShapeType } from 'three-to-cannon';
+import { threeToCannon } from './utils/three-to-cannon';
 import CannonDebugger from 'cannon-es-debugger'
+import * as _ from 'lodash';
 
 onMounted (()=>{
 
     // 场景初始化
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x88ccee);
-    scene.fog = new THREE.Fog(0x88ccee, 0, 50);
+    // scene.fog = new THREE.Fog(0x88ccee, 0, 50);
     // 创建相机
-    const camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.001,
-      1000
-    )
-    camera.position.set(0, 5, 10)
+    const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1010);
+    camera.position.set(0, 350, 120)
     // 渲染
     const container = document.getElementById("container");
-    const renderer = new THREE.WebGL1Renderer({ antialias: true })
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
@@ -54,9 +52,8 @@ onMounted (()=>{
     // 创建物理世界
     const world = new CANNON.World();
     world.gravity.set(0, -100, 0);
-    const cannonDebugger = new CannonDebugger(scene, world, {
-      // options...
-    })
+
+    const cannonDebugger = new CannonDebugger(scene, world, {})
 
  
     let robotModel = null
@@ -66,33 +63,105 @@ onMounted (()=>{
     dracoLoader.setDecoderConfig({ type: "js" });
     dracoLoader.preload();
     gltfLoader.setDRACOLoader(dracoLoader);
-    gltfLoader.load('./models/metaScene03.glb', gltf => {
-      console.log("gltf", gltf)
-      //阴影
-      gltf.scene.traverse(function (object) {
-     
-        if (object.isMesh) object.castShadow = true;
-        if(object.isMesh){
-          console.log(object.geometry)
-          if(object.name === "KB3D_DLA_BldgMD_H_BaseA_1"){
-            object.material.wireframe = true
-            // var shape = new CANNON.Trimesh(object.geometry);
-            // var shape = new CANNON.Heightfield(object.geometry.position.Array);
-            var shape = new CANNON.Trimesh(object.geometry.position.Array);
-            var body = new CANNON.Body({ mass: 1 });
-            body.addShape( shape );
-            // 将物体添加至物理世界
-            world.addBody(body);
-          }
+    gltfLoader.load('./models/world.glb', gltf => {
+      gltf.scene.traverse((child) => {
+        if (child.hasOwnProperty('userData')){
+            if (child.type === 'Mesh'){
+                // Utils.setupMeshProperties(child);
+                // this.sky.csm.setupMaterial(child.material);
+
+                // if (child.material.name === 'ocean')
+                // {
+                //   this.registerUpdatable(new Ocean(child, this));
+                // }
+            }
+
+            if (child.userData.hasOwnProperty('data')){
+                if (child.userData.data === 'physics')
+                {
+                  if (child.userData.hasOwnProperty('type')) 
+                  {
+                    // Convex doesn't work! Stick to boxes!
+                    if (child.userData.type === 'box'){
+                      // let phys = new BoxCollider({size: new THREE.Vector3(child.scale.x, child.scale.y, child.scale.z)});
+                      // phys.body.position.copy(Utils.cannonVector(child.position));
+                      // phys.body.quaternion.copy(Utils.cannonQuat(child.quaternion));
+                      // phys.body.computeAABB();
+
+                      // phys.body.shapes.forEach((shape) => {
+                      //   shape.collisionFilterMask = ~CollisionGroups.TrimeshColliders;
+                      // });
+
+                      // this.physicsWorld.addBody(phys.body);
+                    } else if (child.userData.type === 'trimesh') {
+                      let phys = getBody(child, {});
+                      // console.log("phys", phys)
+                      world.addBody(phys);
+                    }
+
+                    child.visible = false;
+                  }
+                }
+
+                if (child.userData.data === 'path')
+                {
+                  // this.paths.push(new Path(child));
+                }
+
+                if (child.userData.data === 'scenario')
+                {
+                  // this.scenarios.push(new Scenario(child, this));
+                }
+            }
+
         }
-      });
+      })
       robotModel = gltf.scene
       // gltf.scene.name = config.name
       // gltf.scene.rotation.set(0, 0, 0)
       // resolve(gltf)
-      scene.add(robotModel)
+      scene.add(gltf.scene)
     })
 
+    const getBody = (mesh, options) => {
+  
+      const meshClone = mesh.clone();
+
+      let defaults = {
+        mass: 0,
+        position: mesh.position,
+        rotation: mesh.quaternion,
+        friction: 0.3
+      };
+      options = setDefaults(options, defaults);
+
+      let mat = new CANNON.Material('triMat');
+      mat.friction = options.friction;
+      var Type = {
+        BOX: 'Box',
+        CYLINDER: 'Cylinder',
+        SPHERE: 'Sphere',
+        HULL: 'ConvexPolyhedron',
+        MESH: 'Trimesh'
+      };
+
+      let shape = threeToCannon(meshClone, {type: Type.MESH});
+
+      // Add phys sphere
+      let physBox = new CANNON.Body({
+        mass: options.mass,
+        position: options.position,
+        quaternion: options.rotation,
+        shape: shape
+      });
+
+      physBox.material = mat;
+      return physBox
+    }
+
+    const setDefaults = (options, defaults) => {
+         return _.defaults({}, _.clone(options), defaults);
+    }
     // 创建物理小球形状
     const sphereShape = new CANNON.Sphere(0.5);
     // 设置物体材质
@@ -161,10 +230,10 @@ onMounted (()=>{
       let delta = clock.getDelta();
       world.step(1 / 60, delta, 10);
       if(robotModel){
-    
         // const result = threeToCannon(robotModel);
         // console.log('robotModel', result)
-        robotModel.position.copy(sphereBody.position)
+        // robotModel.position.copy(sphereBody.position)
+        sphereBody.position.copy(robotModel.position)
       }
     
       // capsule.quaternion.copy(sphereBody.position)
